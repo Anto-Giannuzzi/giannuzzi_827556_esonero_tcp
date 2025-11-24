680b0b0ff59f1c3a1fafe4fbdc1e1cee6f7c9f68
Anto-Giannuzzi <antonellogiannuzzi2@gmail.com>
2025-11-24 09:29:42 +0100

    completato il programma

--- server-project/src/main.c
+++ server-project/src/main.c
@@ -9,6 +9,9 @@
 
 #if defined WIN32
 #include <winsock.h>
+
+typedef int socklen_t;
+#define strcasecmp _stricmp
 #else
 #include <string.h>
 #include <unistd.h>
@@ -17,6 +20,7 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <netdb.h>
+#include <strings.h>
 #define closesocket close
 #endif
 
@@ -25,104 +29,76 @@
 #include <time.h>
 #include "protocol.h"
 
+
+#ifndef NO_ERROR
 #define NO_ERROR 0
-weather_request_t request;
-weather_response_t response;
+#endif
 
 static const char *SUPPORTED_CITIES[] = {
-	"Bari",
-	"Roma",
-	"Milano",
-	"Napoli",
-	"Torino",
-	"Palermo",
-	"Genova",
-	"Bologna",
-	"Firenze",
-	"Venezia"
+	"Bari", "Roma", "Milano", "Napoli", "Torino",
+	"Palermo", "Genova", "Bologna", "Firenze", "Venezia"
 };
 
-void clearwinsock()
-{
+void errorhandler(char *errorMessage) {
+    printf("%s\n", errorMessage);
+}
+
+void clearwinsock() {
 #if defined WIN32
 	WSACleanup();
 #endif
 }
 
-void valida(char tipo,char* citta )
-{
-	int flag=1;
-	for (int i=0;i<10;i++)
-	{
-		if(strcmp(citta,SUPPORTED_CITIES[i])==0)
-		{
-			flag=0;
+float random_float(float min, float max) {
+	float scale = rand() / (float) RAND_MAX;
+	return min + scale * (max - min);
+}
+
+
+void valida(weather_request_t *req, weather_response_t *resp) {
+
+	if(req->type != 't' && req->type != 'h' && req->type != 'w' && req->type != 'p') {
+		resp->status = 2;
+		return;
+	}
+
+
+	int flag = 1;
+	for (int i = 0; i < 10; i++) {
+		if(strcasecmp(req->city, SUPPORTED_CITIES[i]) == 0) {
+			flag = 0;
+			break;
 		}
 	}
-	if(flag==1)
-	{
-		response.status=1;
-	}else if(tipo!='t' && tipo!='h' && tipo!='w' && tipo!='p')
-	{
-		response.status=2;
-	}else
-	{
-		response.status=0;
+
+	if(flag == 1) {
+		resp->status = 1;
+	} else {
+		resp->status = 0;
 	}
-
 }
 
+float get_temperature(void) { return random_float(-10.0, 40.0); }
+float get_humidity(void)    { return random_float(20.0, 100.0); }
+float get_wind(void)        { return random_float(0.0, 100.0); }
+float get_pressure(void)    { return random_float(950.0, 1050.0); }
 
-float get_temperature(void)
-{
-	float min_value=-10.0;
-	float max_value=40.0;
-	float temp = rand() % (max_value - min_value + 1)+ min_value;
-	return temp;
-}
-float get_humidity(void)
-{
-	float min_value=20.0;
-	float max_value=100.0;
-	float hum = rand() % (max_value - min_value + 1)+ min_value;
-	return hum;
-}
-float get_wind(void)
-{
-	float min_value=0.0;
-	float max_value=100.0;
-	float wind = rand() % (max_value - min_value + 1)+ min_value;
-	return wind;
-}
-float get_pressure(void)
-{
-	float min_value=950.0;
-	float max_value=1050.0;
-	float pres = rand() % (max_value - min_value + 1)+ min_value;
-	return pres;
-}
-
-
-
-int main(int argc, char *argv[])
-{
-
+int main(int argc, char *argv[]) {
 	#if defined WIN32
-		// Initialize Winsock
 		WSADATA wsa_data;
-		int result = WSAStartup(MAKEWORD(2,2), &wsa_data);
-		if (result != NO_ERROR)
-		{
+		if (WSAStartup(MAKEWORD(2,2), &wsa_data) != 0) {
 			printf("Error at WSAStartup()\n");
 			return 0;
 		}
 	#endif
 
-	int my_socket;
+	int port = SERVER_PORT;
+	if (argc > 2 && strcmp(argv[1], "-p") == 0) {
+		port = atoi(argv[2]);
+	}
 
-	my_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (my_socket < 0)
-	{
+	int my_socket = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (my_socket < 0) {
 		errorhandler("socket creation failed.\n");
 		clearwinsock();
 		return -1;
@@ -131,112 +107,70 @@ int main(int argc, char *argv[])
 	struct sockaddr_in sad;
 	memset(&sad, 0, sizeof(sad));
 	sad.sin_family = AF_INET;
-	sad.sin_addr.s_addr = inet_addr("127.0.0.1");
-	sad.sin_port = htons(SERVER_PORT);
+	sad.sin_addr.s_addr = htonl(INADDR_ANY);
+	sad.sin_port = htons(port);
 
-	if (bind(my_socket, (struct sockaddr*) &sad, sizeof(sad)) < 0)
-	{
+	if (bind(my_socket, (struct sockaddr*) &sad, sizeof(sad)) < 0) {
 		errorhandler("bind() failed.\n");
 		closesocket(my_socket);
 		clearwinsock();
 		return -1;
 	}
 
-	if (listen(my_socket, QUEUE_SIZE) < 0)
-	{
+	if (listen(my_socket, QUEUE_SIZE) < 0) {
 		errorhandler("listen() failed.\n");
 		closesocket(my_socket);
 		clearwinsock();
 		return -1;
 	}
 
-	struct sockaddr_in cad; // structure for the client address
-	int client_socket;       // socket descriptor for the client
-	int client_len;          // the size of the client address
-	printf("Waiting for a client to connect...\n\n");
-	while (1)
-	{
-		client_len = sizeof(cad); // set the size of the client address
-		if ((client_socket = accept(my_socket, (struct sockaddr*) &cad,
-				&client_len)) < 0)
-		{
+	struct sockaddr_in cad;
+	int client_socket;
+	int client_len;
+
+	srand(time(NULL));
+	printf("Server in ascolto sulla porta %d...\n", port);
+
+	while (1) {
+		client_len = sizeof(cad);
+
+		if ((client_socket = accept(my_socket, (struct sockaddr*) &cad, (socklen_t*)&client_len)) < 0) {
 			errorhandler("accept() failed.\n");
-			// close connection
-			closesocket(client_socket);
-			clearwinsock();
-			return 0;
+			continue;
 		}
 
-		printf("Handling client %s\n", inet_ntoa(cad.sin_addr));
-
-		char *s = "Connection established";
-		if (send(client_socket, s, strlen(s), 0) != strlen(s))
-		{
-			errorhandler("send() sent a different number of bytes than expected");
+		weather_request_t request;
+		if (recv(client_socket, (char*)&request, sizeof(request), 0) <= 0) {
 			closesocket(client_socket);
-			clearwinsock();
-			return -1;
+			continue;
 		}
 
+		printf("Richiesta '%c %s' dal client ip %s\n", request.type, request.city, inet_ntoa(cad.sin_addr));
 
-		if ((recv(client_socket, &request, sizeof(weather_response_t), 0)) <= 0)
-		{
-			errorhandler("recv() failed or connection closed prematurely");
-			closesocket(client_socket);
-			clearwinsock();
-			return -1;
-		}
+		weather_response_t response;
+		valida(&request, &response);
 
-		printf("Richiesta '%c %s' dal client ip %s\n",request.type, request.city, sad.sin_addr.s_addr);
-
-		valida(request.type,request.city);
-
-		if(response.status==0)
-		{
-			srand(time(NULL));
-			switch (request.type)
-			{
-			case 't':
-				response.value=get_temperature();
-				break;
-
-			case 'h':
-				response.value=get_humidity();
-				break;
-
-			case 'w':
-				response.value=get_wind();
-				break;
-
-			case 'p':
-				response.value=get_pressure();
-				break;
+		if(response.status == 0) {
+			switch (request.type) {
+				case 't': response.value = get_temperature(); break;
+				case 'h': response.value = get_humidity(); break;
+				case 'w': response.value = get_wind(); break;
+				case 'p': response.value = get_pressure(); break;
 			}
-			response.type=request.type;
-		}
-		else
-		{
-			response.type='\0';
-			response.value=0.0;
+			response.type = request.type;
+		} else {
+			response.type = '\0';
+			response.value = 0.0;
 		}
 
-
-
-
-
-
-		if (send(client_socket, &response, sizeof(response), 0) != sizeof(response))
-		{
-			errorhandler("send() sent a different number of bytes than expected");
-			closesocket(client_socket);
-			clearwinsock();
-			return -1;
+		if (send(client_socket, (char*)&response, sizeof(response), 0) != sizeof(response)) {
+			errorhandler("send() failed");
 		}
 
-		printf("Server terminated.\n");
-
-		closesocket(my_socket);
-		clearwinsock();
-		return 0;
+		closesocket(client_socket);
 	}
-} // main end
+
+	closesocket(my_socket);
+	clearwinsock();
+	return 0;
+}
--- server-project/src/protocol.h
+++ server-project/src/protocol.h
@@ -8,30 +8,31 @@
 #ifndef PROTOCOL_H_
 #define PROTOCOL_H_
 
-// Shared application parameters
-#define SERVER_PORT 27015  // Server port (change if needed)
-#define BUFFER_SIZE 512    // Buffer size for messages
-#define QUEUE_SIZE 5       // Size of pending connections queue
+#define SERVER_PORT 56700
+#define DEFAULT_IP "127.0.0.1"
+#define BUFFER_SIZE 512
+#define QUEUE_SIZE 5
 
 typedef struct {
-    char type;        // Weather data type: 't', 'h', 'w', 'p'
-    char city[64];    // City name (null-terminated string)
+    char type;        // 't', 'h', 'w', 'p'
+    char city[64];    // Nome città
 } weather_request_t;
 
 typedef struct {
-    unsigned int status;  // Response status code
-    char type;            // Echo of request type
-    float value;          // Weather data value
+    unsigned int status;
+    char type;
+    float value;
 } weather_response_t;
 
-// Function prototypes
-// Add here the signatures of the functions implemented by students
 
-void valida(char tipo,char* citta );
+void valida(weather_request_t *req, weather_response_t *resp);
+void errorhandler(char *errorMessage);
+void clearwinsock();
+float random_float(float min, float max);
 
-float get_temperature(void);    // Range: -10.0 to 40.0 °C
-float get_humidity(void);       // Range: 20.0 to 100.0 %
-float get_wind(void);           // Range: 0.0 to 100.0 km/h
-float get_pressure(void);       // Range: 950.0 to 1050.0 hPa
+float get_temperature(void);
+float get_humidity(void);
+float get_wind(void);
+float get_pressure(void);
 
 #endif /* PROTOCOL_H_ */
